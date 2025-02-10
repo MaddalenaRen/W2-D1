9462547414450250daaaade4e0dafabb86ad6ec0
MaddalenaRen <ileniarennella@gmail.com>
2025-02-10 22:34:46 +0100

    Update D1.js

--- D1.js
+++ D1.js
@@ -91,6 +91,6 @@ let name2 = "John";
 
 console.log(name1 === name2);
 console.log(name1 !== name2);
-console.log(name1 === name2.toLowerCase());
+console.log(name1.toLowerCase() === name2.toLowerCase())
 
 
